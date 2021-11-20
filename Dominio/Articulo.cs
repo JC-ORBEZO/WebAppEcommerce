d494a1fc6cf419c7f6823154cc8519882a333264
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-11-20 16:33:05 -0300

    Agregue la Clase Articulo.

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        //public Marca marca { get; set; }
+        //public string descripcionMarca { get; set; }
+        //public Categoria categoria { get; set; }
+        //public string descripcionCategoria { get; set; }
+        public string Url { get; set; }
+        public decimal precio { get; set; }
+        public int stock { get; set; }
+        public int limiteStock { get; set; }
+        public string urlImagen { get; set; }
+        public bool activo { get; set; }
+    }
+}
